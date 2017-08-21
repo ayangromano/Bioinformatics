f20be249e6e7295b0f68806eac116fabcd1459be
ayangromano <ayangromano@users.noreply.github.com>
2017-08-21 09:46:29 -0400

    Update efetch_taxonomy.py

--- efetch_taxonomy.py
+++ efetch_taxonomy.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python
+#!/usr/local/bin/python
+
+##This script is not production ready and will need to be fixed/improved upon. It works by taking your blast file, using esearch 
+##and efetch to grab the taxonomy information for all of your hits. However, it isn't a nicely written script. Will work on this!
 
 from Bio import Entrez
 from Bio import SeqIO
@@ -7,11 +10,12 @@ Entrez.email=''
 
 tax_dict = {}
 
-acc_file='top20_tig00000572_blastx.txt'
+#enter name of accession file here
+acc_file=''
 
 def get_accnumber(file):
 
-    "This function grabs the accession number from a blast file. All unique accession numbers are placed in a list"
+    """This function grabs the accession number from a blast file. All unique accession numbers are placed in a list"""
 
 
     with open (file, 'r') as acc:
@@ -23,7 +27,7 @@ def get_accnumber(file):
 
 def esearch(accession_number):
 
-    """esearch"""   
+    """this uses esearch to get the id for your accession numbers"""   
 
     search = Entrez.esearch(db="protein", term=accession_number)
     record = Entrez.read(search)
@@ -33,6 +37,8 @@ def esearch(accession_number):
 
 def efetch(uid):
 
+    """This uses efetch to fetch the taxnomy information for each uniprot id"""
+    
     fetch = Entrez.efetch(db="protein", id=uid, rettype="gb", retmode="text")
     gb = SeqIO.read(fetch, 'genbank')
     if len(gb.annotations['taxonomy']) != 0:
@@ -44,6 +50,7 @@ for acc in get_accnumber(acc_file):
     search = esearch(acc)
     tax_dict[acc]=efetch(search)
 
+#if you have specific name for your output file, replace it with 'output.txt' 
 with open (acc_file, 'r') as afile, open('output.txt', 'w') as tfile:
     for line in afile:
         line=line.split("\t")
@@ -65,30 +72,3 @@ with open (acc_file, 'r') as afile, open('output.txt', 'w') as tfile:
         tfile.write(newline)
 
 
-"""   
-for acc in get_accnumber(acc_file):
-    search = esearch(acc)
-    tax_dict[acc]=efetch(search)
-    print acc+'\t'+tax_dict[acc]
-
-with open(acc_file, 'r') as afile, open('manta_blastx_040316_tax_added.txt', 'w') as tfile:
-    for line in afile:
-        line=line.split("\t")
-        query=line[0]
-        target=line[1]
-        percent_identity=line[2]
-        alignment_length=line[3]
-        num_mismatches=line[4]
-        num_gapopens=line[5]
-        start_pos_in_query=line[6]
-        end_pos_in_query=line[7]
-        start_pos_target=line[8]
-        end_pos_target=line[9]
-        eval=line[10]
-        bit_score=line[11]
-        acc_num=target.split("|")[3]
-        newline=tax_dict[acc_num]+'\t'+query+'\t'+target+'\t'+percent_identity+'\t'+alignment_length+'\t'+num_mismatches+'\t'+num_gapopens+'\t'+start_pos_in_query+'\t'+end_pos_in_query+'\t'+start_pos_target+'\t'+end_pos_target+'\t'+eval+'\t'+bit_score
-        print query, tax_dict[acc_num]
-        
-#tfile.write(newline)
-"""
